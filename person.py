5c06d5f25ead4c761846bea8f5e12f145e021069
Then <6923403@qq.com>
2020-02-29 21:55:33 +0800

    person

---
+++ person.py
@@ -0,0 +1,7 @@
+def person(name, age, **kw):
+    '''print('name: ', name, 'age: ', age, 'other: ', kw)'''
+    if 'city' in kw:
+        pass
+    if 'job' in kw:
+        pass
+    print('name: ', name, 'age: ', age, 'other: ', kw)
