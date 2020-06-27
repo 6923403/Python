32dc049a672f28ee2193c3e4ee788c65c29318d2
Then <vcvckw@gmail.com>
2020-06-27 15:32:31 +0800

    Open Capture, Color grayscale display

---
+++ opencv/openc1.py
@@ -0,0 +1,28 @@
+# 打开摄像头并灰度化显示
+import cv2
+import time
+
+
+def main():
+    capture = cv2.VideoCapture(0)
+    width, height = capture.get(3), capture.get(4)
+    print(width, height)
+
+    # 以原分辨率的一倍来捕获
+   # capture.set(cv2.CAP_PROP_FRAME_WIDTH, width * 2)
+   # capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height * 2)
+    while(True):
+        # 获取一帧
+        ret, frame = capture.read()
+        # 将这帧转换为灰度图
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+if __name__ == '__main__':
+    stime = time.process_time()
+    main()
+    end = time.process_time()
+    print(end - stime)
