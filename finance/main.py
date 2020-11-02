7737fa4a79d3cb78e22ce9f2fc1ae3b975f1e303
Vcvc <vcvckw@gmail.com>
2020-11-02 21:49:43 +0800

    init

--- finance/main.py
+++ finance/main.py
@@ -1,4 +1,19 @@
 from wangyi_fiance import WangYi
+from TongHuaShun import TongHua
+import os, shutil
+import time
+
+def filemove():
+    filename = "C:\\Users\\Vcvc\\Desktop\\News_Finance.xlsx"
+
+    path = "C:\\Users\\Vcvc\\Desktop\\Finance\\"
+    isExists = os.path.exists(path)
+    if not isExists:
+        os.mkdir(path)
+    filetime = time.strftime("%Y_%m_%d_%H_%M", time.localtime()) #year-month-day-hour-minute
+    path2 = path + "Finance_" + filetime + ".xlsx"
+    shutil.move(filename, path2)
+
 
 def Wy():
     Wy = WangYi()
@@ -6,10 +21,10 @@ def Wy():
 
 def TongHuaShun():
     Th = TongHua()
-
+    Th.main()
 
 if __name__ == '__main__':
-    Wy()
+    #Wy()
     TongHuaShun()
-    #Jrj()
+    #filemove()
 
